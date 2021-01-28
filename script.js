111b4ab56f46cafed256de5857706c5a12032d9a
Bryan Molk <brybrym92@MacBook-Pro-3.cable.rcn.com>
2021-01-28 18:49:51 -0500

    gleam like a sapphire

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-// https://gis.cityofboston.gov/arcgis/rest/services/EnvironmentEnergy/OpenData/MapServer/4/query?outFields=*&where=1%3D1
-// https://opendata.arcgis.com/datasets/547a3ccb7ab443ceaaba62eef6694e74_4.geojson
+
 async function restaurants() {
   axios({
     method: "GET",
     // url: "https://cors-anywhere.herokuapp.com/https://developers.zomato.com/api/v2.1",
-    url: "https://developers.zomato.com/api/v2.1/search?entity_id=289&entity_type=city&order=asc",
-    // url: https://developers.zomato.com/api/v2.1/search?entity_id=289&start=0&count=100&sort=rating&order=desc,
+    url: "https://developers.zomato.com/api/v2.1/search?entity_id=289&entity_type=city&order=asc/https://developers.zomato.com/api/v2.1/search?entity_id=289&start=0&count=100&sort=rating&order=desc",
+    // url: "https://developers.zomato.com/api/v2.1/search?entity_id=289&start=0&count=100&sort=rating&order=desc",
+   
     headers: {
       "user-key": "9d76e594aa790973097bf60d5815e470",
       "content-type": "application/json"
     }
-  })
-    .then(response => {
-      console.log(response.data.restaurants)
+  }).then(response => {
+      // console.log(response.data.restaurants)
       const restaurantList = response.data.restaurants
-      restGetData(restaurantList)
+    restGetData(restaurantList)
+    rateGetData(ratedList)
     })
     .catch(error => {
       console.log(error);
@@ -27,16 +27,15 @@ function restGetData(restaurantList) {
   restaurantList.forEach(restaurantList => {
     let newRes = document.createElement('div')
     newRes.classList.add('each-res')
-
     const restaurantData = `
     <h6>Name: ${restaurantList.restaurant.name}</h6> 
     <a href="${restaurantList.restaurant.photos_url}">photos</a>
     <a href="${restaurantList.restaurant.phone_numbers}">Call Us</a>
     <a href="${restaurantList.restaurant.url}">Website</a>
+    <p>Cuisine: ${restaurantList.restaurant.cuisines}</p>
+    <p>Hours: ${restaurantList.restaurant.timings}</p>
     <h6>Address: ${restaurantList.restaurant.location.address}</h6>
-    `
-    
-    // console.log(restContainer)
+    ` // console.log(restContainer)
    restContainer.appendChild(newRes)
    newRes.insertAdjacentHTML('beforeend', restaurantData)
     return restaurantData;
@@ -47,15 +46,30 @@ function restGetData(restaurantList) {
 const searchButton = document.querySelector('.rest-but');
   searchButton.addEventListener('click', (e) => {
     e.preventDefault();
-    removeRes()
-    const inputVal = document.querySelector('.input-class').value  
+    removeRestaurants()
+    const searchValue = document.querySelector('#input-class').value
     // console.log(inputVal)
-    restaurants(inputVal)
+    restaurants(searchValue)
     
   })
 
-function removeRes() {
+function searchSel(restaurantList) {
+    let input=document.querySelector('#input-class').value.toLowerCase();
+    let output=document.querySelector('#realitems').options;
+    for(let i=0;i<output.length;i++) {
+      if(output[i].value.indexOf(input)==0){
+        output[i].selected=true;
+      }
+      if(document.querySelector('#input-class').value==''){
+        output[0].selected=true;
+      }
+    }
+  }
+//  searchSel() 
+function removeRestaurants() {
   const restsContainer = document.querySelector('#rest-list')
   while (restsContainer.lastChild)
     restsContainer.removeChild(restsContainer.lastChild)
 }
+
+document.getElementById("copyright").appendChild(document.createTextNode(new Date().getFullYear()));
